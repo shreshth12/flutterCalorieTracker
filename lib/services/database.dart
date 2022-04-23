c8a3e9632e08e12ccdff5117b777b521697592e2
Shreshth Kaushik <shreshthsoccer12@gmail.com>
2022-04-23 19:11:11 -0400

    Now the app refreshes everyday with clean state

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,5 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+deleteData() async {
+  final instance = FirebaseFirestore.instance;
+  final batch = instance.batch();
+  var collection = instance.collection('tracker');
+  var snapshots = await collection.get();
+  for (var doc in snapshots.docs) {
+    batch.delete(doc.reference);
+  }
+  await batch.commit();
+}
 
 addDataToUser(double calculatedBMR, String uid) {
   String res = "Some error occured";
@@ -19,6 +31,9 @@ addDataToUser(double calculatedBMR, String uid) {
 freshCalorieSetter(
     var user, var calories, var carbs, var fats, var protein, var foodName) {
   String res = "Some error occured";
+  var now = new DateTime.now();
+  var formatter = new DateFormat('yyyy-MM-dd');
+  String formattedDate = formatter.format(now);
   try {
     FirebaseFirestore.instance.collection('tracker').add({
       "user": user,
@@ -26,7 +41,8 @@ freshCalorieSetter(
       "carbs": carbs,
       "fats": fats,
       "protein": protein,
-      "foodName": foodName
+      "foodName": foodName,
+      'datetime': formattedDate,
     });
     res = 'success';
   } catch (err) {
